e755e6daaa7f97405f1d250dfad821513942eb7f
Rahafsaq <rahafalqahtani11@hotmail.com>
2023-11-23 15:17:00 +0300

    edit

--- React-git/src/App.jsx
+++ React-git/src/App.jsx
@@ -1,8 +1,7 @@
-
- import Footer from './componenet/Footer'
 import './App.css';
 import BootAndProg from './componenet/BootAndProg';
 import Card from './componenet/Card';
+import Footer from './componenet/Footer';
 import NavBar from './componenet/NavBar';
 function App() {
 	return (
@@ -13,11 +12,10 @@ function App() {
 				<Card type='معسكر' title='js' startDate='29/11/2023' />
 				<Card type='برنامج' title='js' startDate='27/11/2023' />
 				<Card type='معسكر' title='flutter' startDate='24/11/2023' />
-      <Footer/>
 			</div>
+			<Footer />
 		</>
 	);
-
 }
 
 export default App;
