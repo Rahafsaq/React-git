0672252e01baa6a12dfdbc0ce30a2ee50e8e96e1
Rahafsaq <rahafalqahtani11@hotmail.com>
2023-11-23 15:07:11 +0300

    add cards

--- React-git/src/App.jsx
+++ React-git/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css';
 import BootAndProg from './componenet/BootAndProg';
+import Card from './componenet/Card';
 import NavBar from './componenet/NavBar';
 function App() {
 	return (
 		<>
 			<NavBar></NavBar>
 			<BootAndProg></BootAndProg>
+			<div className='card-container'>
+				<Card type='معسكر' title='js' startDate='29/11/2023' />
+				<Card type='برنامج' title='js' startDate='27/11/2023' />
+				<Card type='معسكر' title='flutter' startDate='24/11/2023' />
+			</div>
 		</>
 	);
 }
